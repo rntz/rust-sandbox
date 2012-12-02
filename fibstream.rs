2d57a06d036461c6e7f0653dce4ca3475f00b8b8
Michael Arntzenius <daekharel@gmail.com>
2012-12-01 20:09:16 -0500

    simplify fibstream.rs

--- fibstream.rs
+++ fibstream.rs
@@ -2,13 +2,10 @@ extern mod rutil;
 use rutil::stream;
 use stream::{Stream,cons,tail,map2,map2_consume};
 
-fn genfibs(fibs: Stream<uint>) -> Stream<uint> {
-  let rest = do map2_consume(fibs, tail(fibs)) |x,y| { x + y };
-  cons(1, cons(1, rest))
-}
-
 fn main() {
-  let fibs = stream::fix(genfibs);
+  let fibs = do stream::fix |fibs| {
+    cons(1, cons(1, do map2_consume(fibs, tail(fibs), |x,y| x + y)))
+  }
   for stream::each(fibs) |x| {
     io::println(fmt!("%u", *x));
   }
