df2404a4b209cf07484e02fa3750d67cadd3247d
Michael Arntzenius <daekharel@gmail.com>
2012-12-01 20:13:43 -0500

    fix & simplify fibstream.rs

--- fibstream.rs
+++ fibstream.rs
@@ -1,12 +1,12 @@
 extern mod rutil;
 use rutil::stream;
-use stream::{Stream,cons,tail,map2,map2_consume};
+use stream::{Stream,cons,tail,map2,map2_consume,fix,each};
 
 fn main() {
-  let fibs = do stream::fix |fibs| {
-    cons(1, cons(1, do map2_consume(fibs, tail(fibs), |x,y| x + y)))
-  }
-  for stream::each(fibs) |x| {
+  let fibs = fix(fn@(fibs: Stream<uint>) -> Stream<uint> {
+    cons(1, cons(1, map2_consume(fibs, tail(fibs), |x,y| x + y)))
+  });
+  for each(fibs) |x| {
     io::println(fmt!("%u", *x));
   }
 }
