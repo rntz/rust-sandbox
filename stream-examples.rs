c3d80673ac71ce78cbfe8000f29c5f6ccd5b0104
Michael Arntzenius <daekharel@gmail.com>
2012-12-02 13:47:00 -0500

    add prime sieve to fibstream.rs, rename to stream-examples.rs

---
+++ stream-examples.rs
@@ -0,0 +1,38 @@
+extern mod rutil;
+use rutil::stream;
+use stream::{Stream,Stream_,Cons,Empty,
+             cons,tail,map,map_consume,map2,map2_consume,fix,each,filter};
+
+// Naturals
+fn nats_from(n: uint) -> Stream<uint> {
+  || Cons(n, nats_from(n+1))
+}
+
+// Fibonaccis
+fn genfibs(fibs: Stream<uint>) -> Stream<uint> {
+  cons(1, cons(1, map2_consume(fibs, tail(fibs), |x,y| x + y)))
+}
+
+// Sieve of Eratosthenes
+fn eratosthenes(s: Stream<uint>) -> Stream<uint> {
+  || match s() {
+    Empty => Empty,
+    Cons(p, xs) => {
+      Cons(p, eratosthenes(filter(xs, |n| *n % p != 0)))
+    }
+  }
+}
+                 
+
+fn main() {
+  let _fibs = fix(genfibs);
+  // for each(fibs) |x| {
+  //   io::println(fmt!("%u", *x));
+  // }
+
+  let odds = map_consume(nats_from(1), |x| 2*x + 1);
+  let primes = cons(2, eratosthenes(odds));
+  for each(primes) |p| {
+    io::println(fmt!("%u", *p));
+  }
+}
