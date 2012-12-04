82299b6fd67faaca9ccb87e1b3cb73e7c5af922d
Michael Arntzenius <daekharel@gmail.com>
2012-12-04 18:33:56 -0500

    more macros

--- macros.rs
+++ macros.rs
@@ -1,10 +1,31 @@
 macro_rules! matches(
-  ($e:expr, $p:pat) => (
+  ($e:expr ~= $p:pat) => (
     match $e { $p => true, _ => false }
   )
 )
 
+macro_rules! and(
+  ($($e:expr),*) => ($($e &&)* true)
+)
+
+macro_rules! case(
+  ($e:expr, $p:pat $(where $cond:expr)* => $thn:expr, $els:expr) => (
+    // FIXME: using option::Some here is an awful (and potentially
+    // inefficiency-introducing) hack to make sure the underscore case isn't
+    // "unreachable".
+    match option::Some($e) {
+      option::Some($p) => if and!($($cond),*) { $thn } else { $els },
+      _ => $els
+    }
+  )
+)
+
 fn main() {
-  assert  matches!(2, 2..3);
-  assert !matches!(4, 2..3);
+  assert  matches!(2 ~= 2..3);
+  assert !matches!(4 ~= 2..3);
+  assert and!(true, true, true);
+  assert !and!(true, false);
+  assert 5 == case!(5, x where x > 3 => x, 3);
+  assert 3 == case!(2, x where x > 3 => x, 3);
+  assert case!(4, x@1..10 where x % 2 == 0 => true, false);
 }
