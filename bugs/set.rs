735c00af56a21d2ededaa782771104b35401ad50
Michael Arntzenius <daekharel@gmail.com>
2012-12-04 22:21:02 -0500

    bugs/set.rs has now been included in libcore :(

--- bugs/set.rs
+++ bugs/set.rs
@@ -6,18 +6,18 @@ fn main() {
   io::println(x.hash_keyed(0,1).to_str());
 }
 
-// from https://github.com/mozilla/rust/pull/4052
-impl<A: IterBytes, B: IterBytes> (A,B): IterBytes {
-  #[inline(always)]
-  pure fn iter_bytes(lsb0: bool, f: to_bytes::Cb) {
-    let (ref a, ref b) = self;
-    a.iter_bytes(lsb0, f);
-    b.iter_bytes(lsb0, f);
-  //   match self {
-  //     (ref a, ref b) => {
-  //       a.iter_bytes(lsb0, f);
-  //       b.iter_bytes(lsb0, f);
-  //     }
-  //   }
-  // }
-}
+// // from https://github.com/mozilla/rust/pull/4052
+// impl<A: IterBytes, B: IterBytes> (A,B): IterBytes {
+//   #[inline(always)]
+//   pure fn iter_bytes(lsb0: bool, f: to_bytes::Cb) {
+//     let (ref a, ref b) = self;
+//     a.iter_bytes(lsb0, f);
+//     b.iter_bytes(lsb0, f);
+//     // match self {
+//     //   (ref a, ref b) => {
+//     //     a.iter_bytes(lsb0, f);
+//     //     b.iter_bytes(lsb0, f);
+//     //   }
+//     // }
+//   }
+// }
